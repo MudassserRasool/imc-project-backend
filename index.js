b2e5c146acb298080330894b7209ee1e309aa02b
Mudasser Rasool <86182941+MudassserRasool@users.noreply.github.com>
2024-01-06 17:39:30 +0500

    changevercael setting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
+import { MONGO_URL } from './config/config.js';
 import orderRouter from './routes/order.js';
 import router from './routes/user.js';
 import workoutRouter from './routes/workout.js';
@@ -9,10 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  'mongodb://mern2022:mern2022@ac-1vocr4t-shard-00-00.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-01.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-02.43aaypx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x4e2nh-shard-0&authSource=admin&retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect(MONGO_URL);
 
 const db = mongoose.connection;
 
@@ -20,9 +18,9 @@ db.on('error', (error) => console.error('MongoDB connection error:', error));
 db.once('open', () => console.log('Connected to MongoDB'));
 
 app.use('/api/workouts', workoutRouter);
-app.use('/api/user', router);
-app.use('/api/orders', orderRouter);
+// app.use('/api/user', router);
+// app.use('/api/orders', orderRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
