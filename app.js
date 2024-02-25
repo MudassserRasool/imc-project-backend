12f0a79d8d83d040af4526e93fd7cd3171c222ce
Mudasser Rasool <86182941+MudassserRasool@users.noreply.github.com>
2024-02-25 20:44:45 +0500

    removed router

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
-import ambulanceRouter from './routes/ambulance.js';
-import customerRouter from './routes/coustomer.js';
-import doctorRouter from './routes/doctor.js';
-import orderRouter from './routes/order.js';
-import overviewRouter from './routes/overview.js';
-import userRouter from './routes/user.js';
 
 const app = express();
 app.use(express.json());
@@ -18,12 +12,12 @@ app.use('/uploads', express.static('uploads'));
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/customers', customerRouter);
-app.use('/api/doctor', doctorRouter);
-app.use('/api/ambulance', ambulanceRouter);
-app.use('/api/overview', overviewRouter);
+// app.use('/api/users', userRouter);
+// app.use('/api/orders', orderRouter);
+// app.use('/api/customers', customerRouter);
+// app.use('/api/doctor', doctorRouter);
+// app.use('/api/ambulance', ambulanceRouter);
+// app.use('/api/overview', overviewRouter);
 
 mongoose
   .connect(process.env.MONGODB_URI)
