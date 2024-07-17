7f6feb3d7a82e51da38ad89818d49f48f785833d
Mudasser Rasool <86182941+MudassserRasool@users.noreply.github.com>
2024-07-17 21:06:21 +0500

    remove console

--- controllers/admin/orderControllers.js
+++ controllers/admin/orderControllers.js
@@ -22,20 +22,4 @@ const getAllOrders = async (req, res) => {
   }
 };
 
-// const deleteOrder = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     if (!mongoose.Types.ObjectId.isValid(id)) {
-//       return res.status(404).send(`No workout with id: ${id}`);
-//     }
-//     const order = await Order.findByIdAndDelete(id);
-//     if (!order) {
-//       return res.status(400).json({ message: 'workout not found' });
-//     }
-//   } catch (error) {
-//     console.error('Error deleting order:', error);
-//     res.status(500).json({ error: 'Error deleting order' });
-//   }
-// };
-
 export { getAllOrders, getOrders };
